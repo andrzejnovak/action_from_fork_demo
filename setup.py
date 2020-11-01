b07a012606f100ff6d0305727592fd9c17daceb6
andrzejnovak <novak5andrzej@gmail.com>
2020-11-02 00:11:44 +0100

    fix: add version to setup

--- setup.py
+++ setup.py
@@ -3,9 +3,25 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        return line
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+print("X", get_version("VERSION"))
+
 setuptools.setup(
     name="testing_gha_dummy", # Replace with your own username
-    version="0.0.1",
+    version = get_version("VERSION"),
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
